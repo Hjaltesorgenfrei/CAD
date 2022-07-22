c27004c1f3d0999de94575b30fcc45f6133fd0dd
Simon Green Kristensen <git@simon-green.dev>
2022-07-22 12:59:10 +0200

    functional-ish wiggle code

--- programs/Linefollowing_Robot/src/main.cpp
+++ programs/Linefollowing_Robot/src/main.cpp
@@ -5,38 +5,59 @@
 #define LEFT_SENSOR_IN 2
 #define RIGHT_SENSOR_IN 4
 #define LED_PIN 13
-#define MAX_SPEED 210 //255 is max
+#define MAX_SPEED 220 // 255 is max
 
-void setup() {
-    pinMode(LEFT_MOTOR_OUT, OUTPUT);
-    pinMode(RIGHT_MOTOR_OUT, OUTPUT);
-    pinMode(LEFT_SENSOR_IN, INPUT);
-    pinMode(RIGHT_SENSOR_IN, INPUT);
-    pinMode(LED_PIN, OUTPUT);
+#define RIGHT true;
+#define LEFT false;
+
+bool direction;
+
+void setup()
+{
+  pinMode(LEFT_MOTOR_OUT, OUTPUT);
+  pinMode(RIGHT_MOTOR_OUT, OUTPUT);
+  pinMode(LEFT_SENSOR_IN, INPUT);
+  pinMode(RIGHT_SENSOR_IN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
+  direction = RIGHT;
 }
 
-void loop() {
+void loop()
+{
   bool leftNotOver = digitalRead(LEFT_SENSOR_IN);
   bool rightNotOver = digitalRead(RIGHT_SENSOR_IN);
 
-  if(leftNotOver) {
-    analogWrite(LEFT_MOTOR_OUT, MAX_SPEED);
+  if (direction)
+  {
+    if (leftNotOver)
+    {
+      analogWrite(LEFT_MOTOR_OUT, MAX_SPEED);
+    }
+    else
+    {
+      analogWrite(LEFT_MOTOR_OUT, 0);
+      direction = LEFT;
+    }
   }
-  else {
-    analogWrite(LEFT_MOTOR_OUT, 0);
-  }
-  
-  if(rightNotOver) {
-    analogWrite(RIGHT_MOTOR_OUT, MAX_SPEED);
-  }
-  else {
-    analogWrite(RIGHT_MOTOR_OUT, 0);
+  else
+  {
+    if (rightNotOver)
+    {
+      analogWrite(RIGHT_MOTOR_OUT, MAX_SPEED);
+    }
+    else
+    {
+      analogWrite(RIGHT_MOTOR_OUT, 0);
+      direction = RIGHT;
+    }
   }
 
-  if (!rightNotOver) {
+  if (direction) // Light if direction set to right
+  {
     digitalWrite(LED_PIN, HIGH);
   }
-  else {
+  else
+  {
     digitalWrite(LED_PIN, LOW);
   }
 }
