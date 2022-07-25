aebf9cb3c1e67d419d0bf346be40e5c35f2c91e7
Hjalte Sorgenfrei Mac Dalland <hjaltesorgenfrei@gmail.com>
2022-07-25 16:04:28 +0200

    Work for today

--- final_project/CameraDolleyCode/src/main.cpp
+++ final_project/CameraDolleyCode/src/main.cpp
@@ -4,20 +4,38 @@
  * then off for one second, repeatedly.
  */
 
-#include <ArduinoJson.hpp>
+#define HW_TIMER_INTERVAL_US      20L
+#define USING_PWM_FREQUENCY     true
+#define USING_TIM_DIV1 true              // for shortest and most accurate timer
+
 #include <ESP8266WebServer.h>
 #include <ESP8266WiFi.h>
+#include <Servo.h>
+#include <ESP8266_PWM.h>
+
+#include <ArduinoJson.hpp>
+
+
 
 const char* ssid = "ESP8266-Access-Point";
 const char* password = "123456789";
 
-IPAddress local_ip(192, 168, 4, 1);
-IPAddress gateway(192, 168, 4, 1);
-IPAddress subnet(255, 255, 255, 0);
+// Init ESP8266Timer
+ESP8266Timer ITimer;
+
+// Init ESP8266_ISR_PWM
+ESP8266_PWM ISR_PWM;
+
+// IPAddress local_ip(192, 168, 4, 1);
+// IPAddress gateway(192, 168, 4, 1);
+// IPAddress subnet(255, 255, 255, 0);
 
 ESP8266WebServer server(80);
 
 int LED1status = LOW;
+
+uint32_t PWM_Pin = D4;
+volatile uint32_t startMicros = 0;
 
 void handle_led1on() {
     LED1status = HIGH;
@@ -41,13 +59,32 @@ void handle_Move() {
         return;
     }
 
-    
     String message = "Body received:\n";
     message += server.arg("plain");
     message += "\n";
 
     server.send(200, "text/plain", message);
     Serial.println(message);
+}
+
+float calculateDutyCycle(int angle) {
+  return 5.0 + ((angle / 180.0) * 5.0);
+}
+
+void handle_Servo() {
+    auto angle = server.arg("angle");
+    String message = "Angle set: ";
+    if (angle != "") {  
+        ISR_PWM.setPWM(PWM_Pin, 20, calculateDutyCycle(angle.toInt()));
+        message += angle.toInt();
+    }
+    message += "\n";
+    server.send(200, "text/plain", message);
+}
+
+void IRAM_ATTR TimerHandler()
+{
+  ISR_PWM.run();
 }
 
 void setup() {
@@ -71,9 +108,20 @@ void setup() {
     server.on("/led1on", handle_led1on);
     server.on("/led1off", handle_led1off);
     server.on("/move", HTTP_POST, handle_Move);
+    server.on("/servo_test", HTTP_POST, handle_Servo);
     server.onNotFound(handle_NotFound);
     server.begin();
 
+    if (ITimer.attachInterruptInterval(HW_TIMER_INTERVAL_US, TimerHandler))
+    {
+      startMicros = micros();
+      Serial.print(F("Starting ITimer OK, micros() = ")); 
+      Serial.println(startMicros);
+    }
+  
+    auto channel = ISR_PWM.setPWM(PWM_Pin, 20.0, 50.0);
+    ISR_PWM.enable(channel);
+    Serial.print("Pwm: "); Serial.println(channel);
     Serial.println("Setup done");
 }
 
---
+++ final_project/Protocol/protocol.md
@@ -0,0 +1,42 @@
+# Protocol
+
+This document describes the protocol for communicating movement to dolly.
+Moves should be sent in the body of a HTTP POST request as plain text.
+
+The moves consists least 1 to be a valid move, ending with a new line character `'\n'`, not `'\r\n'`.
+
+## Parts
+
+The parts in each line need to be sent in the order presented here.
+Time must always be sent.
+At least one of Slide Percent, Pan Angle and Tilt Angle need to be present for anything to move, if Time is sent alone, then the dolly merely waits in place, before executing the next move.
+Each part is presented by a full caps string key, followed by number value and delimiting with a space.
+
+```MOVE_CODE
+TIME 100 SLIDE 10 INTERPOLATION 2
+```
+
+### Time
+
+- Key: TIME
+- Value: Time the move should take in ms. If the value is too low or high the value will be clamped to the range: TBD-TBD.
+- Result: The total time the move should be executed over, changes will be interpolated using the desired method.
+
+### Slide Percent
+
+- Key: SLIDE
+- Value: Between 0 and `max_steps` which can be found by querying `GET /max_steps`, if `max_steps` is wrong it can be calibrated by sending `POST /calibrate_slide` which recount the number of steps. Values outside this range will be clamped to it.
+- Result: The Gantry will move to the step position.
+
+### Pan Angle
+
+- Key: PAN
+- Value: 
+
+### Tilt Angle
+
+- Key: TILT
+
+### Interpolation Method
+
+- Key: INTERPOLATION
