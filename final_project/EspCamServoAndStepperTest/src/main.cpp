cd1a4b81e2a8cd9d137bda8b5e49b67a34645ff2
Hjalte Sorgenfrei Mac Dalland <hjaltesorgenfrei@gmail.com>
2022-08-05 15:05:18 +0200

    Add led flash server

--- final_project/EspCamServoAndStepperTest/src/main.cpp
+++ final_project/EspCamServoAndStepperTest/src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 #include <ESP32Servo.h>
+#include <WiFi.h>
+#include <WebServer.h>
 
 // create a servo object
 Servo servoTilt;
@@ -38,36 +40,68 @@ Servo servoPan;
 #include "A4988.h"
 A4988 stepper(MOTOR_STEPS, DIR, STEP);
 
-int count; // Initialize to the movement we do back from the wall
+int count;  // Initialize to the movement we do back froscehamticm the wall
 
-void calibrateStepper()
-{
-  int count = 0;
-  bool runLeft = true;
-  while (runLeft)
-  {
-    stepper.move(4 * MICROSTEPS);
-    count += 4 * MICROSTEPS;
-    runLeft = !digitalRead(LEFT_STOP);
-  }
+const char* ssid = "ESP_Cam";
+const char* password = "29292929";
 
-  stepper.move(-count);
+WebServer server(80);
 
-  bool runRight = true;
-  while (runRight)
-  {
-    stepper.move(-4 * MICROSTEPS);
-    count += 4 * MICROSTEPS;
-    runRight = !digitalRead(RIGHT_STOP);
-  }
+boolean LEDStatus = LOW;
 
-  Serial.printf("I can count to %d!", count);
-  stepper.move(count / 2);
+void handleLedOn() {
+  LEDStatus = HIGH;
+  digitalWrite(4, LEDStatus);
+  server.send(200, "text/plain", "Led On"); 
 }
 
-void writeTilt(int deg)
-{
-  servoTilt.write(constrain(deg - TILT_MIN, 0, TILT_LIMIT));
+void handleLedOff() {
+  LEDStatus = LOW;
+  digitalWrite(4, LEDStatus);
+  server.send(200, "text/html", "Led On"); 
+}
+
+void handleNotFound() {
+  server.send(404, "text/html", "Not Found"); 
+}
+
+void setupWebServer() {
+	boolean result = WiFi.softAP(ssid, password, 13);
+	if (result) {
+		Serial.println("Ready");
+	} else {
+		Serial.println("Failed!");
+	}
+	server.on("/led_on", handleLedOn);
+	server.on("/led_off", handleLedOff);
+	server.onNotFound(handleNotFound);
+	server.begin();
+}
+
+void calibrateStepper() {
+	int count = 0;
+	bool runLeft = true;
+	while (runLeft) {
+		stepper.move(4 * MICROSTEPS);
+		count += 4 * MICROSTEPS;
+		runLeft = !digitalRead(LEFT_STOP);
+	}
+
+	stepper.move(-count);
+
+	bool runRight = true;
+	while (runRight) {
+		stepper.move(-4 * MICROSTEPS);
+		count += 4 * MICROSTEPS;
+		runRight = !digitalRead(RIGHT_STOP);
+	}
+
+	Serial.printf("I can count to %d!", count);
+	stepper.move(count / 2);
+}
+
+void writeTilt(int deg) {
+	servoTilt.write(constrain(deg - TILT_MIN, 0, TILT_LIMIT));
 }
 
 int prevTilt, nextTilt, currentTilt;
@@ -76,66 +110,65 @@ int prevMove, nextMove, moveAmount;
 
 int pos;
 
-void setup()
-{
-  /*
-   * Set target motor RPM.
-   */
-  Serial.begin(9600);
-  delay(20);
-  stepper.begin(RPM, MICROSTEPS);
-  // if using enable/disable on ENABLE pin (active LOW) instead of SLEEP uncomment next line
-  // stepper.setEnableActiveState(LOW);
-  stepper.setSpeedProfile(stepper.LINEAR_SPEED, 6000, 6000);
-  stepper.enable();
-  servoTilt.setPeriodHertz(50);
-  servoPan.setPeriodHertz(50);
-  pinMode(LEFT_STOP, INPUT_PULLDOWN);
-  pinMode(RIGHT_STOP, INPUT_PULLDOWN);
-  pinMode(BUILTIN_FLASH, OUTPUT);
-  servoTilt.attach(servoTiltPin, minUs, maxUs);
-  delay(500);
-  servoPan.attach(servoPanPin, minUs, maxUs);
-  delay(500);
+void setup() {
+	/*
+	 * Set target motor RPM.
+	 */
+	Serial.begin(9600);
+	delay(20);
+	stepper.begin(RPM, MICROSTEPS);
+	// if using enable/disable on ENABLE pin (active LOW) instead of SLEEP uncomment next line
+	// stepper.setEnableActiveState(LOW);
+	stepper.setSpeedProfile(stepper.LINEAR_SPEED, 6000, 6000);
+	stepper.enable();
+	servoTilt.setPeriodHertz(50);
+	servoPan.setPeriodHertz(50);
+	pinMode(LEFT_STOP, INPUT_PULLDOWN);
+	pinMode(RIGHT_STOP, INPUT_PULLDOWN);
+	pinMode(BUILTIN_FLASH, OUTPUT);
+	servoTilt.attach(servoTiltPin, minUs, maxUs);
+	delay(500);
+	servoPan.attach(servoPanPin, minUs, maxUs);
+	delay(500);
 
-  pos = 90;
-  writeTilt(pos);
-  delay(500);
-  servoPan.write(pos);
-  delay(500);
+	pos = 90;
+	writeTilt(pos);
+	delay(500);
+	servoPan.write(pos);
+	delay(500);
 
-  calibrateStepper();
-  delay(200);
+	calibrateStepper();
+	delay(200);
 }
 
-void loop()
-{
-  /*
-   * We do a little jig both ways from the middle, resetting on the same place
-   */
-  // delay(500);
-  // stepper.rotate(360);
-  // stepper.rotate(-2 * 360);
-  // stepper.rotate(360);
-  // servoTilt.writeMicroseconds(minUs);
-  // servoTilt.write(90);
-  // delay(5);
-  // for (pos = TILT_MIN; pos <= TILT_MAX; pos += 1)
-  for (; pos <= 180; pos += 1)
-  { // sweep from 0 degrees to 180 degrees
-    // stepper.move(4 * MICROSTEPS);
-    writeTilt(pos);
-    servoPan.write(pos);
-    // Serial.println(pos);
-    delay(20);
-  }
-  // for (pos = TILT_MAX; pos >= TILT_MIN; pos -= 1)
-  for (pos = 180; pos >= 0; pos -= 1)
-  { // sweep from 180 degrees to 0 degrees
-    // stepper.move(-4 * MICROSTEPS);
-    writeTilt(pos);
-    servoPan.write(pos);
-    // Serial.println(pos);
-    delay(20);
-  }
+void loop() {
+	/*
+	 * We do a little jig both ways from the middle, resetting on the same place
+	 */
+	// delay(500);
+	// stepper.rotate(360);
+	// stepper.rotate(-2 * 360);
+	// stepper.rotate(360);
+	// servoTilt.writeMicroseconds(minUs);
+	// servoTilt.write(90);
+	// delay(5);
+	// for (pos = TILT_MIN; pos <= TILT_MAX; pos += 1)
+	
+	for (; pos <= 180; pos += 1) {  // sweep from 0 degrees to 180 degrees
+		// stepper.move(4 * MICROSTEPS);
+		writeTilt(pos);
+		servoPan.write(pos);
+		// Serial.println(pos);
+		delay(20);
+		server.handleClient();
+	}
+	// for (pos = TILT_MAX; pos >= TILT_MIN; pos -= 1)
+	for (pos = 180; pos >= 0; pos -= 1) {  // sweep from 180 degrees to 0 degrees
+		// stepper.move(-4 * MICROSTEPS);
+		writeTilt(pos);
+		servoPan.write(pos);
+		// Serial.println(pos);
+		delay(20);
+		server.handleClient();
+	}
 }
